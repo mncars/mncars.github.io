b5698ddccad628a8cbf3b9a175b56939a14611cb
Juanjo <juanjo.lopez@biko2.com>
2014-06-19 18:06:16 +0200

    Si navegador es IE quito animación para que no falle

--- js/main.js
+++ js/main.js
@@ -200,25 +200,26 @@ $(document).ready(function() {
 		offset: '90%',
 		triggerOnce: true
 	});*/
-	
-	var waypointClass = '[class*="animate"]';
-	var animationClass = 'fadeInUp';
-	var delayTime;
-	$(waypointClass).css({opacity: '0'});
-	
-	$(waypointClass).waypoint(function() {
-		delayTime += 100;
-		$(this).delay(delayTime).queue(function(next){
-			$(this).toggleClass('animated');
-			$(this).toggleClass(animationClass);
-			delayTime = 0;
-			next();
-		});
-	},
-	{
-		offset: '90%',
-		triggerOnce: true
-	});
+	if (!navigator.userAgent.match(/msie/i) ) {
+        var waypointClass = '[class*="animate"]';
+        var animationClass = 'fadeInUp';
+        var delayTime;
+        $(waypointClass).css({opacity: '0'});
+
+        $(waypointClass).waypoint(function () {
+                delayTime += 100;
+                $(this).delay(delayTime).queue(function (next) {
+                    $(this).toggleClass('animated');
+                    $(this).toggleClass(animationClass);
+                    delayTime = 0;
+                    next();
+                });
+            },
+            {
+                offset: '90%',
+                triggerOnce: true
+            });
+    }
 });
 
 
